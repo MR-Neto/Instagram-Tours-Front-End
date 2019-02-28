76771c60c0b2c9dbe7b4d2c370726f74228e29d3
Miguel Ribeiro Neto <miguelribeironeto@MacBook-Pro-de-Miguel.local>
2019-02-28 19:45:27 +0100

    Added React Semantics

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,43 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withAuth } from '../components/AuthProvider';
+import { Dropdown, Icon, Menu } from 'semantic-ui-react'
+
 
 class Navbar extends Component {
+
+  MenuExampleAttached = () => (
+    <div>
+      <Menu attached='top'>
+        <Dropdown item icon='bars' simple>
+          <Dropdown.Menu>
+            <Dropdown.Item>
+              <Icon name='dropdown' />
+              <span className='text'>New</span>
+
+              <Dropdown.Menu>
+                <Dropdown.Item>Document</Dropdown.Item>
+                <Dropdown.Item>Image</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown.Item>
+            <Dropdown.Item>
+              <Link to="/">Home</Link>
+            </Dropdown.Item>
+            <Dropdown.Item>
+              <Link to="/book">Book tour</Link>
+            </Dropdown.Item>
+            <Dropdown.Item>Save...</Dropdown.Item>
+            <Dropdown.Item>Edit Permissions</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Header>Export</Dropdown.Header>
+            <Dropdown.Item>Share</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
+      </Menu>
+    </div>
+  )
+
   render() {
     const { isLogged, logout } = this.props;
 
@@ -21,10 +56,36 @@ class Navbar extends Component {
       return (
         <nav>
           <button>Back</button>
+
+          <Menu attached='top'>
+            <Dropdown item icon='bars' simple>
+              <Dropdown.Menu>
+                <Dropdown.Item>
+                  <Icon name='dropdown' />
+                  <span className='text'>New</span>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item>Document</Dropdown.Item>
+                    <Dropdown.Item>Image</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  <Link to="/">Home</Link>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  <Link to="/book">Book tour</Link>
+                </Dropdown.Item>
+                <Dropdown.Item>Save...</Dropdown.Item>
+                <Dropdown.Item>Edit Permissions</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Header>Export</Dropdown.Header>
+                <Dropdown.Item>Share</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Menu>
           <h1>Tours</h1>
           <a href="#" onClick={logout}>Menu</a>
-          <Link to="/">Home</Link>
-          <Link to="/book">Book tour</Link>
+
           <Link to="/auth/login">Log In</Link>
           <Link to="/auth/signup">Sign Up</Link>
         </nav>
